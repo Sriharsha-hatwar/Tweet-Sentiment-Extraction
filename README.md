5c87dfad3dd2be4c59a374136bf3dcdf980f84c4
Sriharsha-hatwar <sriharsha02hatwar@gmail.com>
2020-05-03 12:01:56 +0530

    This commit adds several implementation using different cross validation scheme and using logger for logging 1. Added a new cross validation scheme - More details in the issues section in the repo 2. Minor change in the .gitignore and Readme.md file 3. Added .gitattributes for overriding the linguistics of the repo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Tweet-Sentiment-Extraction
 Ongoing competition in Kaggle.  
 
-Important notice : This is not a naive sentiment prediction of tweets. This system extracts the sentiment causing context words from a tweet by modelling this as a Question Answering task.
+*Important notice* : This is not a naive sentiment prediction task of tweets. This system extracts the sentiment causing context words from a tweet by modelling this as a Question Answering task.
 
 This is a repository for tracking and maintaining the progress in the competetion with checkpoints and source code.
--- src/create_folds.py
+++ src/create_folds.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import logging 
 from sklearn.model_selection import KFold
 
 def create_folds(n_folds = 5):
@@ -7,14 +8,32 @@ def create_folds(n_folds = 5):
     y = train_df.selected_text
     train_df = train_df.sample(frac=1).reset_index(drop=True)
     train_df.loc[ : ,  'kfold'] = -1
-    kf = KFold(n_splits=n_folds)
-    print("Verfication : ",kf.get_n_splits(X))
+    kf = KFold(n_splits = n_folds)
+    logging.info("Verfication : ",kf.get_n_splits(X))
     for fold, (train_indices, val_indices) in enumerate(kf.split(X)):
-        print("The train index : ",train_indices)
-        print("Valid index : ", val_indices)
+        logging.info("The train index : ",train_indices)
+        logging.info("Valid index : ", val_indices)
         train_df.loc[val_indices, 'kfold'] = fold
     
     train_df.to_csv('../input/tweet-sentiment-extraction/train_folds.csv')
 
+def create_sentiment_wise_folds(n_folds = 5):
+    train_df = pd.read_csv('../input/tweet-sentiment-extraction/train.csv')
+    sentiment_df_container = {}
+    sentiments = train_df.sentiment.value_counts()
+    for sentiment in sentiments.keys():
+        X = train_df[train_df['sentiment'] == sentiment].reset_index()
+        X.loc[:, 'kfold'] = -1
+        kf = KFold(n_splits = n_folds)
+        for fold, (train_indices, val_indices) in enumerate(kf.split(X)):
+            X.loc[val_indices, 'kfold'] = fold
+        sentiment_df_container[sentiment] = X
+    sentiment_list = list(sentiment_df_container.keys())
+    final_df = pd.concat([sentiment_df_container[sentiment_list[0]], sentiment_df_container[sentiment_list[1]], 
+                sentiment_df_container[sentiment_list[2]]], axis=0)
+    final_df = final_df.sample(frac=1).reset_index(drop=True)
+    final_df.to_csv('../input/tweet-sentiment-extraction/train_sentiment_folds.csv')
+
 if __name__ == "__main__":
     create_folds(5)
+    create_sentiment_wise_folds(5)
